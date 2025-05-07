0e608231eb8a7b32335c87e1e8255e44d6971234
beniman8 <benik@live.ca>
2025-05-06 20:44:28 -0400

    first commit

---
+++ python/hello_world.py
@@ -0,0 +1,4 @@
+'''This is the first program built in the python programming language.... A start of a journey'''
+
+
+print("Hello World")
---
+++ python/itertools/count.py
@@ -0,0 +1,6 @@
+'''Cool thing using itertools to count'''
+from itertools import count
+
+
+for i in count(20,2):
+    print(i)
