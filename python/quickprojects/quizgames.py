a0500d74da845058b8e2d089861f2e7d273bd395
beniman8 <benik@live.ca>
2025-08-30 22:58:51 -0400

    quiz game

---
+++ python/quickprojects/quizgames.py
@@ -0,0 +1,56 @@
+# small python quiz game
+
+questions = (
+    "How many elements in the periodic table?: ",
+    "Which animal lays the largest eggs?: ",
+    "What is the most abundant gas in Earth's atmosphere?: ",
+    "How may bones in the human body?: ",
+    "Which planet in the solar system is the hottest?: ",
+)
+
+options = (
+    ("A. 116", "B. 117", "C. 118", "D. 119"),
+    ("A. answer 1", "B. answer 2", "C. answer 3", "D. answer 4"),
+    ("A. answer 1", "B. answer 2", "C. answer 3", "D. answer 4"),
+    ("A. answer 1", "B. answer 2", "C. answer 3", "D. answer 4"),
+    ("A. answer 1", "B. answer 2", "C. answer 3", "D. answer 4"),
+)
+
+answers = ("C","D","A","A","B")
+
+guesses = [] 
+
+score = 0
+question_num = 0
+
+for question in questions:
+    print("-----------------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A,B,C,D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score +=1
+        print("CORRECT!")
+    else:
+        print("INCORRECT")
+        print(f"{answers[question_num]} is the correct answer")
+        
+    question_num+=1
+
+
+
+print("answers: ", end=" ")
+for answer in answers:
+    print(answer,end=" ")
+    
+    
+print("guesses: ", end=" ")
+for guess in guesses:
+    print(guess,end=" ")
+print()    
+
+score = int(score / len(questions) * 100) 
+print(f"your score is: {score}%")
