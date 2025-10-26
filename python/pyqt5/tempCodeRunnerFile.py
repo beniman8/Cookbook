f68a9321bd2d3518a9501cea9da21e777a2c0fab
beniman8 <benik@live.ca>
2025-10-26 15:44:49 -0400

    learning pyqt5

---
+++ python/pyqt5/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+        # #Use a custom font
+        # font_id = QFontDatabase.addApplicationFont("DS-DIGIT.TTF")
+        # font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        # my_font = QFont(font_family, 300)
+        # self.time_label.setFont(my_font)
